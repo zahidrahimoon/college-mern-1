37c3bb550e36105f229afd23fc048a3ddbcff3bf
zahidrahimoon <zahidrahimoon22@gmail.com>
2025-02-07 10:44:54 -0800

    updated

--- frontend/src/Pages/StudentTimeTable.jsx
+++ frontend/src/Pages/StudentTimeTable.jsx
@@ -1,6 +1,7 @@
 // import CommerceTable from "../components/CommerceTable";
 import Footer from "../components/Footer";
 import ScheduleTable from "../components/ScheduleTable";
+import ScheduleTableForMedXi from "../components/ScheduleTableForMedXi";
 
 const StudentTimeTable = () => {
   return (
@@ -9,7 +10,7 @@ const StudentTimeTable = () => {
       <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/commercexii`} title="Commerce (XII) Time Table" />
       <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/engxi`} title="ENG (XI) Time Table" />
       <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/engxii`} title="ENG (XII) Time Table" />
-      <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/medxi`} title="MED (XI) Time Table" />
+      <ScheduleTableForMedXi apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/medxi`} title="MED (XI) Time Table" />
       <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/medxii`} title="MED (XII) Time Table" />
       <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/genscixi`} title="GENSCI (XI) Time Table" />
       <ScheduleTable apiEndpoint={`${import.meta.env.VITE_BASE_URL}/api/genscixii`} title="GENSCI (XII) Time Table" />
